0cf150a0109460c5722b8030d03d75822d9aada6
Glitch (hello-express) <none>
2021-03-15 08:55:58 +0000

    👱👙 Checkpoint ./komutlar/yardım.js:5419919/1460 ./komutlar/erkekrol.js:5419919/1040 ./komutlar/kızrol.js:5419919/920

---
+++ komutlar/erkekrol.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "erkekrol",
+  aliases: ['erkek-rol'],
+  code: `$setServerVar[erkek;$message[1]]
+  $argsCheck[>1;Lütfen bir rol belirleyin. Örneğin: *$getServerVar[prefix]erkekrol @rol** ]
+  $title[Rol Kaydedildi]
+  $description[Erkek Rolü Başarıyla **$mentionedRoles[1]** olarak kaydedildi.
+  
+  Rolü Değiştirmek için aynı komutu yeni rol için uygulayın.
+  Kaydeden Yetkili : $username[$authorID]
+  Rol : $mentionedRoles[1]]
+  $footer[$username;$authorAvatar]
+  $addTimeStamp
+  $color[303136]
+  $onlyPerms[admin;Rolü Kaydetmek İçin **Yönetici** Yetkisine Sahip Olmanız Gerekmektedir.]
+  $onlyIf[$getServerVar[kayıtsistemi]==açık;Bu komutu kullanmak için kayıt sistemini açmanız gerekiyor. Açmak için **$getServerVar[prefix]kayıtsistemi aç** yazmalısınız.]
+  $deletecommand
+  $deleteIn[10s]`
+};
+
+//Bu komutu tamamen Falsis Kremlin Yazmıştır. Çalınması Kesinlikle Yasaktır.
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -19,5 +19,24 @@ module.exports = ({
   **$getServerVar[prefix]kayıtyapıldı-kanal #kanal**
   - Birisi Kayıt olunca Mesaj Atılacak olan kanal. Genelde Chat (sohbet) kanalı olarak ayarlanır.
   
-  `
+  **$getServerVar[prefix]tag \`taginiz\`**
+  - Birisi Kayıt olunca isminin başına girilecek etiket (zorunlu değil)
+  
+    **$getServerVar[prefix]ayraç \`ayracınız\`**
+  - Birisi Kayıt olunca ismi ve yaşının ortasına girilecek etiket (zorunlu değil ama ayarlanmadığı takdirde **|** olur.) Örneğin : Kerem | 14
+  
+    **$getServerVar[prefix]otorol-aç @rol**
+  - Birisi Sunucuya girince verilecek rol. (zorunlu)
+  
+    **$getServerVar[prefix]erkekrol @rol**
+  - Birisi Kayıt olunca verilecek **erkek** rolü
+  
+      **$getServerVar[prefix]kızrol @rol**
+  - Birisi Kayıt olunca verilecek **kız** rolü
+  
+      **$getServerVar[prefix]e @kişi \`isim\` \`yaş\`**
+  - Birisini kayıt etmek için kullanılır (erkek)
+  
+        **$getServerVar[prefix]k @kişi \`isim\` \`yaş\`**
+  - Birisini kayıt etmek için kullanılır (kız)`
 })
